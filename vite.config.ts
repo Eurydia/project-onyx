4027d7363be845dd0f6fb63cb0b2d10305a5ea8f
Thanakorn Phuttharaksa <crossdeltavi@gmail.com>
2026-07-29 06:52:04 +0700

    updated dependencies to avoid security issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/project-onyx/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      cache: true,
-      treeshake: true,
-      output: {
-        compact: true,
-        manualChunks: (id) => {
-          if (id.includes("node_modules"))
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-        },
-      },
-    },
-
-    sourcemap: true,
-  },
   assetsInclude: ["**/*.md"],
+  resolve: {},
 });
